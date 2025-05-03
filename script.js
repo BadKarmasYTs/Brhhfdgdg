60beffdb7856873c0289103c53f2359ee8855d49
BadKarmasYTs <lukezuskas@gmail.com>
2025-05-02 21:55:21 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,16 +2,15 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/fireba
 import { getDatabase, ref, push, onValue, update } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-database.js";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
+  apiKey: "AIzaSyCuwF7_iRb0CIqApyk3_bBFPCoUHVTbn1Q",
+  authDomain: "website-e08f0.firebaseapp.com",
+  databaseURL: "https://website-e08f0-default-rtdb.firebaseio.com",
+  projectId: "website-e08f0",
+  storageBucket: "website-e08f0.firebasestorage.app",
+  messagingSenderId: "738977165779",
+  appId: "1:738977165779:web:8b54334e8306aa5932bf26",
+  measurementId: "G-5DC7NWVJGD"
 };
-
 
 const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
